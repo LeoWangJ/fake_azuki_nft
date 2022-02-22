4a00ee7d12e8464849aa80e4410bfe5b434d7481
leowang <rfv7855659@gmail.com>
2022-02-23 05:04:09 +0800

    ✨ add auction mint

--- contracts/Leo.sol
+++ contracts/Leo.sol
@@ -3,6 +3,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "erc721a/contracts/ERC721A.sol";
 import "erc721a/contracts/extensions/ERC721AOwnersExplicit.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 /**
  * 需求:
@@ -20,12 +21,22 @@ import "erc721a/contracts/extensions/ERC721AOwnersExplicit.sol";
  */
 
 contract Leo is Ownable, ERC721A, ERC721AOwnersExplicit, ReentrancyGuard {
+    using SafeMath for uint256;
     uint256 immutable maxPerMintSize;
     uint256 immutable totalSize;
     uint256 immutable devMintSize;
     uint256 immutable auctionAndDevMintSize;
     string private _baseURI;
     string public merkleRoot;
+
+    struct SaleConfig {
+        uint64 publicPrice;
+        uint32 publicStartTime;
+        uint32 whitePrice;
+        uint64 auctionStartTime;
+    }
+
+    SaleConfig public saleConfig;
 
     constructor(
         uint256 _maxPerMintSize,
@@ -52,11 +63,46 @@ contract Leo is Ownable, ERC721A, ERC721AOwnersExplicit, ReentrancyGuard {
     }
 
     function devMint(uint256 quantity) external onlyOwner {
-      require(totalSupply() + quantity <= totalSize, "too many already minted before dev mint");
-      require(quantity <= devMintSize,"too many dev mint");
-      require(quantity % maxPerMintSize == 0, "dev mint can only Mint maxPerMintSize multiples");
-      _safeMint((msg.sender, quantity);
-      
+        require(
+            totalSupply() + quantity <= totalSize,
+            "too many already minted before dev mint"
+        );
+        require(
+            _numberMinted(msg.sender).add(quantity) <= devMintSize,
+            "too many dev mint"
+        );
+        require(
+            quantity % maxPerMintSize == 0,
+            "dev mint can only Mint maxPerMintSize multiples"
+        );
+        _safeMint(msg.sender, quantity);
+    }
+
+    function auctionMint(uint256 quantity) external payable callerIsUser {
+        uint256 startTime = uint256(saleConfig.auctionStartTime);
+
+        require(
+            startTime != 0 && block.timestamp >= startTime,
+            "sale has not start yet"
+        );
+        require(
+            _numberMinted(msg.sender).add(quantity) <= maxPerMintSize,
+            "can not mint this quantity"
+        );
+        require(
+            totalSupply().add(quantity) <= auctionAndDevMintSize,
+            "not enough remaining reserved for auction to support desired mint amount"
+        );
+        uint256 totalPrice = getAuctionPrice(startTime).mul(quantity);
+        _safeMint(msg.sender, quantity);
+        refundIfOver(totalPrice);
+    }
+
+    function refundIfOver(uint256 price) private {
+        require(msg.value >= price, "need to send more ETH.");
+        if (msg.value > price) {
+            payable(msg.sender).transfer(msg.value - price);
+        }
     }
 
     uint256 public constant AUCTION_START_PRICE = 0.1 ether;
@@ -66,4 +112,17 @@ contract Leo is Ownable, ERC721A, ERC721AOwnersExplicit, ReentrancyGuard {
     uint256 public constant AUCTION_DROP_PER_STEP =
         (AUCTION_START_PRICE - AUCTION_END_PRICE) /
             (AUCTION_TOTAL_TIME / AUCTION_DROP_TIME);
+
+    function getAuctionPrice(uint256 startTime) public view returns (uint256) {
+        if (block.timestamp < startTime) {
+            return AUCTION_START_PRICE;
+        }
+
+        if (block.timestamp - startTime >= AUCTION_TOTAL_TIME) {
+            return AUCTION_END_PRICE;
+        } else {
+            uint256 steps = (block.timestamp - startTime) / AUCTION_DROP_TIME;
+            return AUCTION_START_PRICE - (steps * AUCTION_DROP_PER_STEP);
+        }
+    }
 }
